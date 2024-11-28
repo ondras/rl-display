cbc1f9300f200c37dd0caf41650d4a52cf5224eb
Ondřej Žára <ondrej.zara@gmail.com>
2024-11-28 23:08:53 +0100

    example tuning

--- manual/index.js
+++ manual/index.js
@@ -1,72 +1,8 @@
-const AsyncGeneratorFunction = (async function*() {}).constructor;
+import ExAmple from "./ex-ample.js"
 
-class ExAmple extends HTMLElement {
-	#result = document.createElement("div");
-	#pre = document.createElement("pre");
-
-	constructor(scriptNode) {
-		super();
-		this.attachShadow({mode:"open"});
-
-		let code = scriptNode.textContent.trim();
-		this.#pre.className = "code";
-		this.#pre.textContent = code;
-
-
-		this.#result.className = "result";
-		//this.#result.append(func());
-	}
-
-	async connectedCallback() {
-		const { shadowRoot } = this;
-		shadowRoot.innerHTML = HTML;
-		shadowRoot.append(this.#result, this.#pre);
-
-		shadowRoot.querySelector("[name=result]").checked = true;
-		shadowRoot.querySelector("[name=code]").checked = true;
-
-		let func = new AsyncGeneratorFunction(this.#pre.textContent);
-
-		let gen = func();
-		for await (let result of gen) {
-			this.#result.append(result);
-		}
-	}
-}
-
-const HTML = `
-<style>
-:host {
-	display: block;
-	background-color: #eee;
-}
-
-* {
-	box-sizing: border-box;
-}
-
-nav {
-	display: flex;
-	justify-content: space-between;
-	label {
-		display: flex;
-		align-items: center;
-	}
-}
-
-nav:has([name=result]:not(:checked)) ~ .result { display: none; }
-nav:has([name=code]:not(:checked)) ~ .code { display: none; }
-</style>
-<nav>
-	<label><input type="checkbox" name="result">result</input></label>
-	<label><input type="checkbox" name="code">code</input></label>
-</nav>
-`;
-
-customElements.define("ex-ample", ExAmple);
 
 function createExample(scriptNode) {
-	let node = new ExAmple(scriptNode);
+	let node = ExAmple.fromScript(scriptNode);
 	scriptNode.replaceWith(node);
 }
 
